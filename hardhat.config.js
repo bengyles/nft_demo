96dc837fcdfb5b75691e0ae951e49e1cd40c6039
bengyles <bengyles@hotmail.com>
2022-11-01 23:34:56 +0100

    Deploy contract on goerli using hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/V2RXpb-pHizhncGQ3-s1oq0a4rEYnULW",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.4",
 };
