424cab77e7524ad12f0a5050547ff56acae6ada6
bengyles <bengyles@hotmail.com>
2022-11-02 14:36:01 +0100

    Improved header and metamask button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
-import {Anchor, Nav, Header as HeaderBlock} from 'grommet';
+import {Anchor, Nav, Header as HeaderBlock, Box} from 'grommet';
 import MetamaskButton from './MetamaskButton';
 import {useMetaMask} from 'metamask-react';
 
 function Header() {
   const {status} = useMetaMask();
   return (
-      <HeaderBlock pad="small">
-        <Nav direction="row" gap="medium">
+      <HeaderBlock  pad="small">
+        <Nav direction="row" gap="medium" alignSelf="stretch" flex={true}>
           <Anchor label="Home" href="/"/>
           <Anchor label="Marketplace" href="/marketplace"/>
           {status === "connected"?<Anchor label="My items" href="/my-items"/>: null}
-          <div>
-            <MetamaskButton/>
-          </div>
         </Nav>
+        <Box align="end">
+          <MetamaskButton/>
+        </Box>
       </HeaderBlock>
   );
 }
--- src/components/MetamaskButton.js
+++ src/components/MetamaskButton.js
@@ -1,16 +1,26 @@
 import { useMetaMask } from "metamask-react";
+import {Button, Text} from 'grommet';
+import {ethers} from 'ethers';
+import {useState} from 'react';
 
 function MetamaskButton(){
-  const { status, connect, account, chainId } = useMetaMask();
+  const { status, connect, account, chainId, ethereum, switchChain} = useMetaMask();
+  const[balance, setBalance] = useState(0);
 
-  if (status === "unavailable") return <div><a target="_blank" href="https://metamask.io/">Install Metamask</a></div>
+  if (status === "unavailable") return <Text><a target="_blank" href="https://metamask.io/">Install Metamask</a></Text>
 
-  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+  if (status === "notConnected") return <Button onClick={connect} label="Connect to MetaMask"/>
 
-  if (status === "connecting" || status === "initializing") return <div>Loading...</div>
+  if (status === "connecting" || status === "initializing") return <Text>Loading...</Text>
 
   if (status === "connected"){
-    return <div>Connected account {account} on chain ID {chainId}</div>
+    if(chainId === process.env.REACT_APP_CHAIN_ID) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      provider.getBalance(account).then(result=> setBalance(ethers.utils.formatEther(result)));
+      return <Text>Connected to {account}, balance: {balance} ETH</Text>
+    }else{
+      return <Text>Wrong chain! <Button label="switch to goerli network" onClick={()=>switchChain(process.env.REACT_APP_CHAIN_ID)} /></Text>
+    }
   }
 
   return null;
